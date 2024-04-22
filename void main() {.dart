da08861900d3272e6d8a66156c31bbf96c05e107
Shahinazmohsen <167243679+Shahinazmohsen@users.noreply.github.com>
2024-04-22 18:30:01 +0200

    Add files via upload

---
+++ void main() {.dart
@@ -0,0 +1,76 @@
+
+import 'dart:svg';
+
+void main(one) {
+bool biggerThan90 = true;
+if(biggerThan90) {
+    print ("fat");
+} bool lessThan50 = true;
+if (lessThan50) {
+    print ("thin");
+} else {
+    print ("normal weight");
+}
+
+}
+
+int dayNumber = 7;
+string dayName = "saturday";
+switch (dayNumber) {
+  case 1:
+  dayName = "sunday";
+    
+    break;
+    case 2:
+    dayName = "monday";
+
+    break;
+    case 3:
+    dayName = "tuesday";
+
+    break;
+    case 4:
+    dayName = "wednesday";
+
+     break;
+     case 5:
+     dayName = "thursday";
+
+     break;
+     case 6:
+     dayName = "friday";
+
+     break;
+     case 7:
+     dayName = "saturday";
+     break;
+
+
+
+  default:
+  dayName ="invalid day";
+  break;
+}
+print (dayName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
